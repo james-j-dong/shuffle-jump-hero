3ddf3389736000c77902c969bf9f60746cdca2e6
James Dong <jamesjd1024@gmail.com>
2025-04-12 18:35:34 -0500

    added embed code

--- src/app/components/generate-embed/generate-embed.component.ts
+++ src/app/components/generate-embed/generate-embed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-generate-embed',
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './generate-embed.component.scss'
 })
 export class GenerateEmbedComponent {
-
+  embedCode: string = '';
+  ngOnInit(): void {
+    this.embedCode = this.generateEmbedCode();
+    
+  }
+  generateEmbedCode(): string {
+    const embedCode = `<iframe src="https://example.com/embed" width="600" height="400" frameborder="0"></iframe>`;
+    return embedCode;
+  }
+  copyToClipboard(): void {  
+    navigator.clipboard.writeText(this.embedCode).then(() => {
+      console.log('Embed code copied to clipboard');
+    }, (err) => {
+      console.error('Could not copy text: ', err);
+    });
+  }
 }
